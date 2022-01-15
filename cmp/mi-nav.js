1a48e7ed523ac6f983c0fc7ae61bba00598d0632
rickmrdam <rickmrdam@gmail.com>
2022-01-15 10:00:07 -0600

    Cambio

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -39,7 +39,7 @@ export class MiNav extends HTMLElement {
      <li>
       <h3>Lecciones</h3>
       <ol>
-       <li>
+      <!-- <li>
         <details>
          <summary>
           <h4>
@@ -66,7 +66,7 @@ export class MiNav extends HTMLElement {
          </summary>
          <fundamentos-contenido></fundamentos-contenido>
         </details>
-       </li>
+       </li>  -->
        <li>
         <details>
          <summary>
